ec193f488298857436a32d7cd1a8bb7c8c8d5ef8
your_github_username <taalaibekuulusanzhar@gmail.com>
2022-01-28 11:57:38 +0600

    Get List of Users by Location

--- src/main/java/com/example/hibernate/controller/LocationController.java
+++ src/main/java/com/example/hibernate/controller/LocationController.java
@@ -1,6 +1,7 @@
 package com.example.hibernate.controller;
 
 import com.example.hibernate.models.Location;
+import com.example.hibernate.models.User;
 import com.example.hibernate.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,5 +25,16 @@ public class LocationController {
     @GetMapping("/location/{id}")
     public Optional<Location> getLocationById(@PathVariable("id") Integer id){
         return locationService.getLocationById(id);
+    }
+
+    @GetMapping("/location/{id}/users")
+    public List<User> getUsersByLocation(@PathVariable("id") Integer id){
+        Optional<Location> location = locationService.getLocationById(id);
+
+        if(location.isPresent()){
+            Location newLocation = location.get();
+            return newLocation.getUsers();
+        }
+        return  null;
     }
 }
