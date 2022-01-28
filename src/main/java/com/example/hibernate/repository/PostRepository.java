2d9954e7a2036008467a3b00fdf428dcdd8182d2
your_github_username <taalaibekuulusanzhar@gmail.com>
2022-01-28 20:38:40 +0600

    get list of Posts by User from PostController by Extending JPARepository

--- src/main/java/com/example/hibernate/repository/PostRepository.java
+++ src/main/java/com/example/hibernate/repository/PostRepository.java
@@ -5,6 +5,10 @@ import com.example.hibernate.models.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post,Integer> {
+    List<Post> findByUserId(Integer id);
+   // Post findByDetails(String details);
 }
--- src/main/java/com/example/hibernate/service/PostService.java
+++ src/main/java/com/example/hibernate/service/PostService.java
@@ -5,11 +5,7 @@ import com.example.hibernate.models.Post;
 import com.example.hibernate.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.persistence.Access;
-import javax.persistence.PostRemove;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +20,9 @@ public class PostService {
 
     public Optional<Post> getPotsById(Integer id){
         return postRepository.findById(id);
+    }
+
+    public List<Post> getPotsByUser(Integer id) {
+     return postRepository.findByUserId(id);
     }
 }
